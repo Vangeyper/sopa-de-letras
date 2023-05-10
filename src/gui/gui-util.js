5e3fed27b9f89f03b689a0e016278ceac54861ce
Vangeyper <vangeyperr@gmail.com>
2023-05-11 00:48:28 +0200

    diseny grid

--- src/gui/gui-util.js
+++ src/gui/gui-util.js
@@ -15,19 +15,13 @@
         <div class="tablero grid-container">      
         `;
         for ( let y = 0; y < dimensionY; y++ ) {
-            html = html + '<div class="filaTablero grid-item-row">';
+            //html = html + '<div class="filaTablero grid-item-row">';
             for( let x = 0; x < dimensionX; x++ ) {                    
                 let valor = valores[(dimensionX*y)+x];
                 html = html + `
-                    <div class="columnaTablero grid-item-column">
-                        <div id="elem${x}_${y}" class="casilla grid-item">
-                            <span class="botonCasilla grid-item-button">${valor}</span>
-                        </div>
-                    </div>                    
+                    <div id="elem${x}_${y}" class="casilla grid-item">${valor}</div>
                     `;                
             }
-            html = html + '</div>';
-            html = html + '<div class="limpiarFlotantes"></div>';
         }
         html = html + '</div>';
     
